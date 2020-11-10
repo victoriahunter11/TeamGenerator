806b14b0fd7f916364073bdb4a0df5b303875620
Victoria Hunter <victoria.jane09@gmail.com>
2020-11-10 15:10:03 -0500

    adding intern and engineer classes

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,2 +1,32 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
- 
+const Employee = require('./Employee') 
+
+class Engineer extends Employee {
+    constructor(name, id, email, officeNumber, gitHub) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.officeNumber = officeNumber;
+        this.gitHub = gitHub;
+        this.role = 'Engineer';
+    }
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getofficeNumber() {
+        return this.officeNumber;
+    }
+    getgitHub() {
+        return this.gitHub;
+    }
+    getRole() {
+        return this.role;
+    }
+
+}
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Employee class
+
+    const Employee = require('./Employee');
+
+    class Intern extends Employee {
+        constructor(name, id, email, school) {
+            this.name = name;
+            this.id = id;
+            this.email = email;
+            this.school = school;
+            this.role = 'Intern';
+        }
+
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getSchool() {
+        return this.school;
+    }
+    getRole() {
+        return this.role;
+    }
+
+}
