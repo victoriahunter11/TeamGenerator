a3803a6937176a366a30ec9db63c04dd0c7bdf2b
Victoria Hunter <victoria.jane09@gmail.com>
2020-11-10 15:06:04 -0500

    updated manager class

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,29 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require('./Employee');
+
+class Manager {
+    constructor(name, id, email, officeNumber) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.officeNumber = officeNumber;
+        this.role = 'Manager';
+    }
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getofficeNumber() {
+        return this.officeNumber;
+    }
+
+    getRole() {
+        return this.role;
+    }
+
+}
