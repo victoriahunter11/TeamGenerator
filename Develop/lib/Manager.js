8bc755fc654fd4cdcab250945b3b3cdc5f5d1308
Victoria Hunter <victoria.jane09@gmail.com>
2020-11-10 15:07:25 -0500

    added an extension for employee and manager

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,7 +1,7 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require('./Employee');
 
-class Manager {
+class Manager extends Employee {
     constructor(name, id, email, officeNumber) {
         this.name = name;
         this.id = id;
