22dd9f0e6262167089b6fb963669201d1091e54c
Victoria Hunter <victoria.jane09@gmail.com>
2020-11-12 14:11:49 -0500

    got rid of comments, and console logs

--- Develop/app.js
+++ Develop/app.js
@@ -13,9 +13,6 @@ const { rejects } = require("assert");
 const { CLIENT_RENEG_LIMIT } = require("tls");
 const staff = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
 const updateManager = () => {
 
     inquirer.prompt([
@@ -71,14 +68,12 @@ const updateStaff = () => {
 
         } else {
 
-           // fs.writeFile(outputPath)
              const html = render(staff);
              fs.writeFile(outputPath, html, error => {
                 if (error) {
-                    console.log('error', error)
+                    
                 }
 
-                console.log('success')
             })
         }
 
@@ -122,7 +117,6 @@ const updateEngineer = () => {
 
 
         const updateIntern = () => {
-            console.log('here');
             inquirer.prompt([
                 {
                     type: "input",
@@ -156,23 +150,3 @@ const updateEngineer = () => {
 
 
                 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! 
