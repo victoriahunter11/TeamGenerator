a407c9ca1deaf2820945d920ca4e28b8b1ce51c4
Victoria Hunter <victoria.jane09@gmail.com>
2020-11-12 07:44:49 -0500

    adding correct javascript and fixing errors with testing

--- Develop/app.js
+++ Develop/app.js
@@ -10,46 +10,119 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const { rejects } = require("assert");
+const { CLIENT_RENEG_LIMIT } = require("tls");
 const staff = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-const updateManager =  () => {
-        
-inquirer  .prompt([
+const updateManager = () => {
 
-       {
-           type: "input",
-           name: "managerName",
-           message: "What is your manager's name?"
-       },
-       {
-        type: "input",
-        name: "managerID",
-        message: "What is your manager's ID?"
-       },
-       {
-        type: "input",
-        name: "managerEmail",
-        message: "What is your manager's email?"
-       },
-       {
-        type: "input",
-        name: "managerOfficeNumber",
-        message: "What is your manager's office number?"
-       },
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is your manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "What is your manager's ID?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is your manager's email?"
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is your manager's office number?"
+        },
     ]).then(answers => {
         const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber)
         staff.push(manager)
         updateStaff();
     })
-
-
 }
 
-updateManager()
-        const addIntern = () => {
+updateManager();
+
+const updateStaff = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "staffJob",
+            message: "Choose what type of employee you are adding.",
+            choices: ["Intern", "Engineer", "Build Team"]
+        }
+    ])
+    .then(answers => {
+
+        console.log('answers.staffJob ', answers.staffJob )
+
+
+        if (answers.staffJob == "Engineer") {
+            updateEngineer();
+
+        } else if (answers.staffJob == "Intern") {
+            updateIntern();
+
+        } else {
+
+           // fs.writeFile(outputPath)
+             const html = render(staff);
+             fs.writeFile(outputPath, html, error => {
+                if (error) {
+                    console.log('error', error)
+                }
+
+                console.log('success')
+            })
+        }
+
+
+    })
+}
+
+const updateEngineer = () => {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: "What is your engineer's name?"
+           },
+           {
+            type: "input",
+            name: "engineerID",
+            message: "What is your engineer's ID?"
+           },
+           {
+            type: "input",
+            name:"engineerEmail",
+            message: "What was your engineer's email?"
+           },
+           {
+            type: "input",
+            name:"engineerGitHub",
+            message: "What is your engineer's GitHub?"
+           }
+
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub)
+            staff.push(engineer)
+            updateStaff();
+           
+        })
+    }
+
+
+
+
+        const updateIntern = () => {
+            console.log('here');
             inquirer.prompt([
                 {
                     type: "input",
@@ -75,69 +148,15 @@ updateManager()
                 ]).then(answers => {
                     const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool)
                     staff.push(intern)
-                    UpdateStaff();
+                    updateStaff();
                 })
             }
 
-            const addEngineer = () => {
-                inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "engineerName",
-                        message: "What is your engineer's name?"
-                       },
-                       {
-                        type: "input",
-                        name: "engineerID",
-                        message: "What is your engineer's ID?"
-                       },
-                       {
-                        type: "input",
-                        name:"engineerEmail",
-                        message: "What was your engineer's email?"
-                       },
-                       {
-                        type: "input",
-                        name:"engineerGitHub",
-                        message: "What is your engineer's GitHub?"
-                       }
-    
-                    ]).then(answers => {
-                        const intern = new Intern(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGitHub)
-                        staff.push(engineer)
-                        updateStaff();
-                    })
-                }
+          
+
+
                 
-                const updateStaff = () => {
-                    return inquirer.prompt([
-                        {
-                            type: "list",
-                            name: "staffJob",
-                            message: "Choose what type of employee you are adding.",
-                            choices: ["Manager", "Intern", "Engineer"] 
-                        }
-                    ])
-                    .then(answers => {
-                        if (answers.staffJob == "Engineer") {
-                            updateEngineer();
 
-                        } else if (answers.updateStaff == "intern") {
-                            updateIntern();
-
-                        } else {
-                            const html = render(staff);
-                            return fs.writeFile('./output/team.html'), html, error => {
-                                if (error) {
-                                    rejects(error);
-                                    return;
-                                }
-                            }
-                        }
-
-                    })
-                }
-                
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
